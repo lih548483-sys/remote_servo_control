123ff450ec7cf0e46bc63cda7a2f3698b0af4069
lih548483-sys <lih548483@gmail.com>
2026-01-06 22:46:52 +0800

    可以正常使用
    代码暂时无误，分两个文件，一个是案例文件，一个是控制舵机文件

--- sketch_dec2a/sketch_dec2a.ino
+++ sketch_dec2a/sketch_dec2a.ino
@@ -1,54 +1,52 @@
-#define BLINKER_PRINT Serial
+// 定义使用无SSL模式以减少代码大小
+#define BLINKER_WITHOUT_SSL
+// 禁用WebSocket注册功能以减少代码大小
+#define BLINKER_WITHOUT_WS_REG
+// 禁用HTTP服务器以减少代码大小
+#undef BLINKER_PROTOCOL_HTTP_SERVER
+// 禁用ArduinoJson功能以减少代码大小
+#undef BLINKER_ARDUINOJSON
+// 禁用智能设备平台支持以减少代码大小
+#undef BLINKER_ALIGENIE
+#undef BLINKER_DUEROS
+#undef BLINKER_MIOT
+// 禁用调试功能以减少代码大小
+#undef BLINKER_LAN_DEBUG
+#undef BLINKER_DEBUG_ALL
 #define BLINKER_WIFI
 
 #include <Blinker.h>
+#include <ESP32Servo.h>
 
-char auth[] = "3f77d0ef843f";
-char ssid[] = "515";
-char pswd[] = "88888888";
+const char auth[] = "3f77d0ef843f";
+const char ssid[] = "515";
+const char pswd[] = "88888888";
 
-// 新建组件对象
 BlinkerButton Button1("btn-abc");
-BlinkerNumber Number1("num-abc");
+Servo myServo;
+bool servoState = false;
 
-int counter = 0;
-const int outputPin = 1; // 使用数字引脚1(D1)作为输出
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 8
+#endif
 
-// 按下按键即会执行该函数
+#define SERVO_PIN 1
+
 void button1_callback(const String & state) {
-    BLINKER_LOG("get button state: ", state);
-    // 在引脚1输出高电平(255对应Arduino的HIGH)
-    digitalWrite(outputPin, HIGH);
-    // 保持内置LED的切换功能(可选)
+    servoState = !servoState;
+    myServo.write(servoState ? 150 : 0);
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
-// 如果未绑定的组件被触发，则会执行其中内容
-void dataRead(const String & data) {
-    BLINKER_LOG("Blinker readString: ", data);
-    counter++;
-    Number1.print(counter);
-}
-
 void setup() {
-    // 初始化串口
-    Serial.begin(115200);
-
-    #if defined(BLINKER_PRINT)
-        BLINKER_DEBUG.stream(BLINKER_PRINT);
-    #endif
-    
-    // 初始化有LED的IO
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH); 
     
-    // 初始化输出引脚1
-    pinMode(outputPin, OUTPUT);
-    digitalWrite(outputPin, LOW); // 初始状态设为低电平
+    myServo.setPeriodHertz(50);
+    myServo.attach(SERVO_PIN, 500, 2500);
+    myServo.write(0);
     
-    // 初始化blinker
     Blinker.begin(auth, ssid, pswd);
-    Blinker.attachData(dataRead);
     Button1.attach(button1_callback);
 }
 
